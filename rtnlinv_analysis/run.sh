08afa43c104d6f0208dc42ad175829ec3d250062
Nick Scholand <scholand@tugraz.at>
2024-12-20 11:43:01 +0100

    Move visualization to correct script

--- rtnlinv_analysis/run.sh
+++ rtnlinv_analysis/run.sh
@@ -162,30 +162,4 @@ nice -n 5 bart sim --STM \
 
 bart join 12 sim{0,1,}
 
-# -------------------------------------
-#    	Visualization
-# -------------------------------------
 
-python3 func/plot_simulation.py results/Bloch_short{,_hp}/t1map ref_roi roi sim results/figure  $(ls vertices/*.cfl | sed -e 's/\.cfl//')
-
-./func/move_results.sh
-
-
-# Improve Testing
-
-# Join ROIs
-bart join 6 $(ls vertices/*.cfl | sed -e 's/\.cfl//') results/rois
-
-# Extract ROIs
-
-bart fmac results/{LL/t1map,rois,LL/t1_test}
-
-bart fmac results/{Bloch_flash/t1map,rois,Bloch_flash/t1_test}
-
-bart fmac results/{Bloch_short/t1map,rois,Bloch_short/t1_test}
-bart fmac results/{Bloch_short/t2map,rois,Bloch_short/t2_test}
-
-bart fmac results/{Bloch_short_hp/t1map,rois,Bloch_short_hp/t1_test}
-bart fmac results/{Bloch_short_hp/t2map,rois,Bloch_short_hp/t2_test}
-
-rm results/rois.{cfl,hdr}
